8d22b00ea9217460903618d805908f6c0dd04cde
soogleb37 <soogleb37@gmail.com>
2021-09-09 12:03:23 +0530

    soogl6

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 console.log('hello world');
 console.log(document.getElementsByTagName("p")[0].innerHTML);
 console.log(document.getElementsByClassName("clsname")[0].innerText);
+function ok(){
+    console.log("clicked");
+    document.getElementsByClassName("clsname")[0].innerText="shiohsnb"
+    document.getElementById("first").style.color="red"
+}
